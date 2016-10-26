a0c3d79297168b4beb73b420c67bc430a0168b27
Wei Song <watkinsong@163.com>
2016-10-26 10:13:21 +0800

    update

--- leetcode/8.cpp
+++ leetcode/8.cpp
@@ -73,3 +73,60 @@ public:
         return number;
     }
 };
+
+class Solution {
+public:
+    int myAtoi(string str) {
+        int idx = 0;
+        while (str[idx] == ' ')
+        {
+            idx++;
+        }
+
+        bool negative = false;
+
+        if (str[idx] == '-')
+        {
+            negative = true;
+            idx++;
+        }
+        else if (str[idx] == '+')
+        {
+            idx++;
+        }
+
+        int num = 0;
+        while (idx < str.size())
+        {
+            int d = str[idx] - 48;
+            if (d < 0 || d > 9)
+            {
+                break;
+            }
+
+            int new_num = num * 10 + d;
+            if (new_num / 10 != num)
+            {
+                // overflow
+                if (negative == true)
+                {
+                    return INT_MIN;
+                }
+                else
+                {
+                    return INT_MAX;
+                }
+            }
+
+            num = new_num;
+            idx++;
+        }
+
+        if (negative == true)
+        {
+            num = -num;
+        }
+
+        return num;
+    }
+};
