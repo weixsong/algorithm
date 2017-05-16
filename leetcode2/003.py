b4482a13e5d63c67b73768f284476372a2bcd8ff
weixsong <watkinsong@163.com>
2017-05-16 11:52:03 +0600

    update

--- leetcode2/003.py
+++ leetcode2/003.py
@@ -16,6 +16,11 @@ Given "pwwkew", the answer is "wke", with the length of 3.
 
 
 class Solution(object):
+    """
+    Use hashset to record what char is covered
+    Time: O(n)
+    Space: O(n)
+    """
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
@@ -48,8 +53,108 @@ class Solution(object):
         return longest
 
 
+class Solution2(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        p1, p2 = 0, 0
+        longest = 0
+
+        collection = set()
+        while p1 < len(s) and p2 < len(s):
+            while p2 < len(s) and s[p2] not in collection:
+                collection.add(s[p2])
+                p2 += 1
+
+            length = p2 - p1
+            if length > longest:
+                longest = length
+
+            if p2 == len(s):
+                return longest
+
+            while p1 < p2 and s[p1] != s[p2]:
+                collection.remove(s[p1])
+                p1 += 1
+
+            collection.remove(s[p1])
+            p1 += 1
+
+        return longest
+
+
+class Solution3(object):
+    """
+    This one is the best
+    Time: O(n)
+    Space: O(n)
+    """
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        p1, p2, longest = 0, 0, 0
+
+        collection = set()
+        while p2 < len(s):
+            c = s[p2]
+            if c not in collection:
+                collection.add(c)
+                p2 += 1
+            else:
+                d = p2 - p1
+                if d > longest:
+                    longest = d
+
+                while c in collection:
+                    collection.remove(s[p1])
+                    p1 += 1
+
+        if p2 - p1 > longest:
+            longest = p2 - p1
+
+        return longest
+
+
+class Solution4(object):
+    """
+    This one is the best
+    Time: O(n)
+    Space: O(n)
+    """
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        p1, p2, longest = 0, 0, 0
+
+        collection = set()
+        while p2 < len(s):
+            c = s[p2]
+            if c not in collection:
+                collection.add(c)
+                p2 += 1
+                d = p2 - p1
+                if d > longest:
+                    longest = d
+            else:
+                while c in collection:
+                    collection.remove(s[p1])
+                    p1 += 1
+
+        return longest
+
+
 if __name__ == '__main__':
-    so = Solution()
+    so = Solution3()
     print(so.lengthOfLongestSubstring("abcabcbb"))
     print(so.lengthOfLongestSubstring("bbbbb"))
     print(so.lengthOfLongestSubstring("pwwkew"))
+    print(so.lengthOfLongestSubstring("c"))
