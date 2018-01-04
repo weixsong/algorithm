6be80b310c93772d09133445fe4e0d329f7b270f
wei song <watkinsong@163.com>
2018-01-04 19:10:10 +0800

    Update 101.py

--- leetcode2/101.py
+++ leetcode2/101.py
@@ -45,3 +45,39 @@ class Solution(object):
             queue = new_queue
 
         return True
+
+
+class Solution2(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
+        if root == None:
+            return True
+
+        stack1 = [root.left]
+        stack2 = [root.right]
+
+        while len(stack1) != 0 and len(stack2) != 0:
+            t1 = stack1.pop()
+            t2 = stack2.pop()
+            
+            if t1 == None and t2 == None:
+                continue
+            elif t1 != None and t2 != None:
+                if t1.val != t2.val:
+                    return False
+
+                stack1.append(t1.left)
+                stack2.append(t2.right)
+
+                stack1.append(t1.right)
+                stack2.append(t2.left)
+            else:
+                return False
+
+        if len(stack1) != len(stack2):
+            return False
+        return True
