5818bd23b3eedb4c9b98dc029dfd9e3967ec4010
weixsong <watkinsong@163.com>
2018-02-27 14:05:50 +0800

    update

--- leetcode2/053.py
+++ leetcode2/053.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 
+"""
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+"""
+
 
 class Solution(object):
+    """
+    Dynamic programming: O(n)
+    f(i): largest sum at step i
+    f(i) = f(i-1) + nums[i] if f(i-1) + nums[i] > nums[i]
+         = nums[i]
+    """
 
     def maxSubArray(self, nums):
         """
